bcc20d49fb0e444c9cdafcdcc8d724847e7e98b0
ngocanvx <lnan.c3vinhxuan@vinhlong.edu.vn>
2026-03-30 10:43:16 +0700

    Cap nhat gui kem ten bai kiem tra

--- js/kiemtra.js
+++ js/kiemtra.js
@@ -799,6 +799,7 @@ document.addEventListener('DOMContentLoaded', () => {
             score_p2: part_score[1],
             score_p3: part_score[2],
             paytime: `${minutes} phút ${seconds} giây`,
+            title: lesson_title,
             ip: clientIP,      // Thêm trường IP
             userAgent: browserInfo // Thêm trường Trình duyệt
         };
@@ -911,6 +912,7 @@ document.addEventListener('DOMContentLoaded', () => {
             formData.append('score_p2', part_score[1]);
             formData.append('score_p3', part_score[2]);
             formData.append('paytime', `${minutes} phút ${seconds} giây`);
+            formData.append('title', lesson_title);
             formData.append('ip', clientIP);
             formData.append('userAgent', browserInfo);
 
